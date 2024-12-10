5a55c6be9bc5bf3c4d838fff63aa8a5ef9e80b20
Martin Petr <git@bodkan.net>
2024-12-10 16:01:29 +0100

    Add a few more wild PCA examples

--- pca.R
+++ pca.R
@@ -9,84 +9,134 @@ library(cowplot)
 library(ggplot2)
 library(viridis)
 
-popA <- population("popA", time = 50000, N = 10000)
-popB <- population("popB", time = 30000, N = 10000, parent = popA)
-popC <- population("popC", time = 20000, N = 10000, parent = popB)
-popD <- population("popD", time = 10000, N = 10000, parent = popC)
-popE <- population("popE", time = 5000, N = 10000, parent = popD)
-popF <- population("popF", time = 2000, N = 10000, parent = popE)
+popAnc <- population("popAnc", time = 2000, N = 5000, remove = 1499)
+popX <- population("popX", time = 1500, N = 5000, parent = popAnc)
+popY <- population("popY", time = 1500, N = 5000, parent = popAnc)
 
-plot_pca <- function(prefix, ts, pc = c(1, 2)) {
+model <- compile_model(list(popAnc, popX, popY), generation_time = 1)
+
+schedule <- schedule_sampling(model, times = seq(2000, 0, by = -100), list(popAnc, 10), list(popX, 10), list(popY, 10))
+
+ts <- msprime(model, samples = schedule, sequence_length = 50e6, recombination_rate = 1e-8) %>% ts_mutate(1e-8)
+
+samples_XY_present <- ts_samples(ts) %>% filter(pop %in% c("popX", "popY"), time == 0)
+ts_simplify(ts, simplify_to = samples_XY_present$name) %>% ts_eigenstrat("model_XY_present")
+
+samples_XY_all <- ts_samples(ts) %>% filter(pop %in% c("popX", "popY"))
+ts_simplify(ts, simplify_to = samples_XY_all$name) %>% ts_eigenstrat("model_XY_all")
+
+samples_AncXY_all <- ts_samples(ts)
+ts_eigenstrat(ts, "model_AncXY_all")
+
+plot_grid(plot_model(model), plot_pca("model_XY_present", samples_XY_present))
+plot_grid(plot_model(model), plot_pca("model_XY_all", samples_XY_all))
+plot_grid(plot_model(model), plot_pca("model_AncXY_all", samples_XY_all))
+
+
+
+plot_pca <- function(prefix, samples, pc = c(1, 2), color = c("time", "pop")) {
   if (length(pc) != 2)
     stop("The 'pc' argument of 'plot_pca' must be an integer vector of length two", call. = FALSE)
 
-  groups <- ts_samples(ts)$pop
-  suppressMessages(pca <- smart_pca(snp_data = paste0(prefix, ".geno"), sample_group = groups))
+  color <- match.arg(color)
+
+  suppressMessages(pca <- smart_pca(snp_data = paste0(prefix, ".geno"), program_svd = "bootSVD", sample_group = samples$pop))
 
   pc_cols <- paste0("PC", pc)
   pca_df <- pca$pca.sample_coordinates[, pc_cols]
-  pca_df$pop <- groups
-  pca_df$time <- ts_samples(ts)$time
+  pca_df$pop <- samples$pop
+  pca_df$time <- samples$time
 
   variance_explained <- pca$pca.eigenvalues[2, ] %>% {. / sum(.) * 100} %>% round(1)
 
+  if (color == "time") {
+    gg_point <- geom_point(aes(x = !!sym(pc_cols[1]), y = !!sym(pc_cols[2]), shape = pop, color = time))
+    gg_theme <- scale_color_viridis_c(option = "viridis")
+  } else {
+    gg_point <- geom_point(aes(x = !!sym(pc_cols[1]), y = !!sym(pc_cols[2]), shape = pop, color = pop))
+    gg_theme <- scale_color_discrete(drop = FALSE)
+  }
+
   ggplot(pca_df) +
-    geom_point(aes(x = !!sym(pc_cols[1]), y = !!sym(pc_cols[2]), shape = pop, color = time)) +
+    gg_point +
+    scale_shape_discrete(drop = FALSE) +
     labs(x = sprintf("%s [%.1f %%]", pc_cols[1], variance_explained[pc[1]]),
          y = sprintf("%s [%.1f %%]", pc_cols[2], variance_explained[pc[2]])) +
     theme_bw() +
-    scale_color_viridis_c()
+    gg_theme
 }
 
-# model without gene flow -------------------------------------------------
-
-model_nogf <- compile_model(list(popA, popB, popC, popD, popE, popF), generation_time = 30)
-
-plot_model(model_nogf)
-
-# gf <- gene_flow(from = popA, to = popF, rate = 0.2, start = 1900, end = 2000)
-# model_nogf <- compile_model(list(popA, popB, popC, popD, popE, popF), generation_time = 1, simulation_length = 2000, gene_flow = gf)
-
-samples_nogf <- schedule_sampling(model_nogf, times = seq(50000, 0, by = -1000), list(popA, 2), list(popB, 2), list(popC, 2), list(popD, 2), list(popE, 2))
-# samples_nogf <- schedule_sampling(model_nogf, times = 2000, list(popA, 25), list(popB, 25), list(popC, 25), list(popD, 25), list(popE, 25), list(popF, 25))
-
-ts_nogf <- msprime(model_nogf, samples = samples_nogf, sequence_length = 10e6, recombination_rate = 1e-8) %>% ts_mutate(1e-8)
-
-ts_eigenstrat(ts_nogf, "pops_ABCDEF_nogf", chrom = "chr1", outgroup = "outgroup")
-
-# comparison --------------------------------------------------------------
-
-plot_grid(plot_model(model_nogf, proportions = TRUE), plot_pca("pops_ABCDEF_nogf", ts_nogf), nrow = 1)
 
 
 
+# gene-flow model ---------------------------------------------------------
+
+devtools::load_all("~/Projects/slendr")
+init_env()
+
+library(smartsnp)
+
+library(dplyr)
+library(scales)
+library(cowplot)
+library(ggplot2)
+library(viridis)
+
+popZ <- population("popZ", time = 3000, N = 5000)
+popX <- population("popX", time = 1500, N = 5000, parent = popZ)
+popY <- population("popY", time = 1500, N = 5000, parent = popZ)
+
+gf <- gene_flow(from = popX, to = popY, rate = 0.5, start = 200, end = 0)
+model <- compile_model(list(popZ, popX, popY), generation_time = 1, gene_flow = NULL)
+
+schedule <- schedule_sampling(model, times = seq(2000, 0, by = -100), list(popZ, 10), list(popX, 10), list(popY, 10))
+
+ts <- msprime(model, samples = schedule, sequence_length = 50e6, recombination_rate = 1e-8) %>% ts_mutate(1e-8)
+
+samples <- ts_samples(ts) %>% mutate(pop = factor(pop, levels = c("popZ", "popX", "popY")))
+
+samples_XYpresent <- samples %>% filter(pop %in% c("popX", "popY"), time == 0)
+ts_simplify(ts, simplify_to = samples_XYpresent$name) %>% ts_eigenstrat("model_XYpresent")
+
+samples_XYall <- samples %>% filter(pop %in% c("popX", "popY"))
+ts_simplify(ts, simplify_to = samples_XYall$name) %>% ts_eigenstrat("model_XYall")
+
+samples_XYZpresent <- samples %>% filter(time == 0)
+ts_simplify(ts, simplify_to = samples_XYZpresent$name) %>% ts_eigenstrat("model_XYZpresent")
+
+samples_XYZall <- samples
+ts_eigenstrat(ts, "model_XYZall")
+
+samples_XYall_Zancient <- samples %>% filter(pop %in% c("popX", "popY") | (pop == "popZ" & time >= 1500))
+ts_eigenstrat(ts, "model_XYall_Zancient")
+
+p_model <- plot_model(model, proportions = TRUE, samples = schedule)
+
+plot_grid(p_model, plot_pca("model_XYpresent", samples_XYpresent, color = "pop"))
+plot_grid(p_model, plot_pca("model_XYall", samples_XYall, color = "time"))
+
+plot_grid(p_model, plot_pca("model_XYZpresent", samples_XYZpresent, color = "pop"))
+
+plot_grid(p_model, plot_pca("model_XYZall", samples_XYZall, pc = c(1, 2)))
+plot_grid(p_model, plot_pca("model_XYZall", samples_XYZall, pc = c(1, 2), color = "pop"))
+plot_grid(p_model, plot_pca("model_XYZall", samples_XYZall, pc = c(2, 3)))
+plot_grid(p_model, plot_pca("model_XYZall", samples_XYZall, pc = c(2, 3), color = "pop"))
+plot_grid(p_model, plot_pca("model_XYZall", samples_XYZall, pc = c(3, 4)))
+plot_grid(p_model, plot_pca("model_XYZall", samples_XYZall, pc = c(3, 4), color = "pop"))
+plot_grid(p_model, plot_pca("model_XYZall", samples_XYZall, pc = c(4, 5)))
+plot_grid(p_model, plot_pca("model_XYZall", samples_XYZall, pc = c(4, 5), color = "pop"))
+
+# when the whole X and Y trajectories are in but only ancient Z lineage, X and Y are
+# not actually separated from one another (only based on time)
+# -- also note that this series seems to kind of replicate the previous series of
+#    figures, except that X and Y are always mingled
+plot_grid(p_model, plot_pca("model_XYall_Zancient", samples_XYall_Zancient, pc = c(1, 2), color = "time"))
+plot_grid(p_model, plot_pca("model_XYall_Zancient", samples_XYall_Zancient, pc = c(1, 2), color = "pop"))
+plot_grid(p_model, plot_pca("model_XYall_Zancient", samples_XYall_Zancient, pc = c(2, 3), color = "pop"))
+plot_grid(p_model, plot_pca("model_XYall_Zancient", samples_XYall_Zancient, pc = c(3, 4), color = "pop"))
+plot_grid(p_model, plot_pca("model_XYall_Zancient", samples_XYall_Zancient, pc = c(4, 5), color = "pop"))
 
 
 
 
 
-
-
-
-
-# simple model ------------------------------------------------------------
-
-popAnc <- population("popAnc", time = 2000, N = 10000, remove = 1499)
-popX <- population("popX", time = 1000, N = 10000, parent = popAnc)
-popY <- population("popY", time = 1000, N = 10000, parent = popAnc)
-
-model_nogf <- compile_model(list(popAnc, popX, popY), generation_time = 1)
-
-# gf <- gene_flow(from = popA, to = popF, rate = 0.2, start = 1900, end = 2000)
-# model_nogf <- compile_model(list(popA, popB, popC, popD, popE, popF), generation_time = 1, simulation_length = 2000, gene_flow = gf)
-
-samples_nogf <- schedule_sampling(model_nogf, times = seq(2000, 0, by = -100), list(popAnc, 10), list(popX, 10), list(popY, 10))
-# samples_nogf <- schedule_sampling(model_nogf, times = 2000, list(popA, 25), list(popB, 25), list(popC, 25), list(popD, 25), list(popE, 25), list(popF, 25))
-
-ts_nogf <- msprime(model_nogf, samples = samples_nogf, sequence_length = 10e6, recombination_rate = 1e-8) %>% ts_mutate(1e-8)
-
-ts_eigenstrat(ts_nogf, "pops_AncXY_nogf", chrom = "chr1", outgroup = "outgroup")
-
-# comparison --------------------------------------------------------------
-
-plot_grid(plot_model(model_nogf, proportions = TRUE), plot_pca("pops_AncXY_nogf", ts_nogf), nrow = 1)
