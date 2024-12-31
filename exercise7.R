c35857080f5c7259915952df7ed77225786e340a
Martin Petr <git@bodkan.net>
2024-12-31 12:27:37 +0100

    Finalize the selection exercsie

--- exercise7.R
+++ exercise7.R
@@ -7,12 +7,12 @@ source("utils.R")
 
 
 
-# part 1 -- building a purely neutral model -------------------------------
+# Part 1 -- building a purely neutral model -------------------------------
 
 # African ancestral population
 afr <- population("AFR", time = 65000, N = 5000)
 
-# first migrants out of Africa
+# First migrants out of Africa
 ooa <- population("OOA", parent = afr, time = 60000, N = 5000, remove = 27000)
 
 # Eastern hunter-gatherers
@@ -27,18 +27,21 @@ ana <- population("ANA", time = 28000, N = 5000, parent = ooa, remove = 4000)
 # Yamnaya steppe population
 yam <- population("YAM", time = 8000, N = 5000, parent = ehg, remove = 2500)
 
-# define gene-flow events
+# Define gene-flow events
 gf <- list(
   gene_flow(from = ana, to = yam, rate = 0.75, start = 7500, end = 6000),
   gene_flow(from = ana, to = eur, rate = 0.5, start = 6000, end = 5000),
   gene_flow(from = yam, to = eur, rate = 0.65, start = 4000, end = 3500)
 )
 
+# Compile all populations into a single slendr model object
 model <- compile_model(
   populations = list(afr, ooa, ehg, eur, ana, yam),
   gene_flow = gf, generation_time = 30
 )
 
+# Schedule the sampling from four European populations roughly before their
+# disappearance (or before the end of the simulation).
 schedule <- rbind(
   schedule_sampling(model, times = 0, list(eur, 50)),
   schedule_sampling(model, times = 6000, list(ehg, 50)),
@@ -46,6 +49,7 @@ schedule <- rbind(
   schedule_sampling(model, times = 2500, list(yam, 50))
 )
 
+# Verify visually that our model is correct
 plot_model(model, samples = schedule)
 plot_model(model, proportions = TRUE)
 
@@ -53,7 +57,7 @@ plot_model(model, proportions = TRUE)
 
 
 
-# part 2 -- simulating a tree sequence and computing Tajima's D -----------
+# Part 2 -- simulating a tree sequence and computing Tajima's D -----------
 
 # Although the point of this exercise is to simulate selection, let's first
 # simulate a normal neutral model using slendr's msprime engine as a sanity check
@@ -78,32 +82,34 @@ ts_tajima(ts, sample_sets = samples)
 
 
 
-# part 3 -- computing Tajima's D in windows -------------------------------
+# Part 3 -- computing Tajima's D in windows -------------------------------
 
-# pre-compute genomic windows for window-based computation of Tajima's D
+# Pre-compute genomic windows for window-based computation of Tajima's D
 windows <- as.integer(seq(0, ts$sequence_length, length.out = 100))
 windows
 
-# compute genome-wide Tajima's D for each population in individual windows
+# Compute genome-wide Tajima's D for each population in individual windows
 tajima_wins <- ts_tajima(ts, sample_sets = samples, windows = windows)
 tajima_wins
 tajima_wins[1, ]$D
 
-# the numeric vector format of the result above is a bit annoying to work with
+# The numeric vector format of the result above is a bit annoying to work with
 # and visualize, so let's use some tidyverse "magic" to format it properly
+# using a helper function `process_tajima()` I wrote for you
 tajima_df <- process_tajima(tajima_wins)
 tajima_df
 
-# now let's visualize the window-based Tajima's D along the simulated genome
-# (hint: we don't actually expect anything interesting here because we ran
-# a purely neutral simulation, so this is more like a control)
+# Now let's visualize the window-based Tajima's D along the simulated genome
+# using another helper function `plot_tajima()` (hint: we still don't expect
+# anything interesting here because we ran a purely neutral simulation, so this
+# is more like a control)
 plot_tajima(tajima_df)
 
 
 
 
 
-# part 4 -- simulating positive selection ---------------------------------
+# Part 4 -- simulating positive selection ---------------------------------
 
 # Our SLiM selection extension for slendr is in 'exercise7_slim.txt'. When
 # you inspect that file, you will se pretty much standard SLiM code with one
@@ -129,9 +135,9 @@ extension
 model <- compile_model(
   populations = list(afr, ooa, ehg, eur, ana, yam),
   gene_flow = gf, generation_time = 30,
-  extension = extension   # <======== this is different to the compilation above!
+  extension = extension   # <======== this is different to the neutral example!
 )
-# We can finally run our selection simulation!
+# We can finally run our selection simulation
 
 # This time our model not only produces a tree sequence, but it also generates
 # a table of allele frequencies in each population (see the file
@@ -144,7 +150,7 @@ path <- slim(model, sequence_length = 10e6, recombination_rate = 1e-8, samples =
 # the table of allele frequencies.
 list.files(path)
 
-# allele frequency trajectories -------------------------------------------
+# Allele frequency trajectories -------------------------------------------
 
 traj_df <- read_trajectory(path)
 traj_df
@@ -161,6 +167,8 @@ plot_grid(
 
 # Tajima's D --------------------------------------------------------------
 
+# We need an additional call to `ts_recapitate()` to ensure that the tree
+# sequence is fully coalesced (and thus contains older polymorphisms)
 ts <- ts_read(file.path(path, "slim.trees"), model) %>%
   ts_recapitate(Ne = 5000, recombination_rate = 1e-8) %>%
   ts_mutate(mutation_rate = 1e-8)
@@ -188,6 +196,9 @@ plot_tajima(tajima_df)
 # Bonus exercises ---------------------------------------------------------
 
 
+
+
+
 # Bonus 1 -----------------------------------------------------------------
 
 # Vary the uniform recombination rate and observe what happens with Tajima's D
@@ -195,6 +206,9 @@ plot_tajima(tajima_df)
 
 # Solution: just modify the value of the `recombination_rate =` argument provided
 # to the `slim()` function above.
+
+
+
 
 
 # Bonus 2 -----------------------------------------------------------------
@@ -210,6 +224,13 @@ extension <- substitute_values(
   s = 0.1,
   onset_time = 12000
 )
+model <- compile_model(
+  populations = list(afr, ooa, ehg, eur, ana, yam),
+  gene_flow = gf, generation_time = 30,
+  extension = extension
+)
+
+
 
 
 
@@ -217,5 +238,3 @@ extension <- substitute_values(
 
 # Re-implement my `process_tajima()` function on your own. Hard mode: do this
 # using base R (no tidyverse allowed!).
-
-
