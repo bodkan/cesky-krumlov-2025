07fb9e2087e1a491c0c1a0342d784ae274001211
Martin Petr <git@bodkan.net>
2025-01-08 12:23:59 +0100

    Flip solution order

--- solutions/solution5.R
+++ solutions/solution5.R
@@ -7,238 +7,115 @@ source(here::here("utils.R"))
 
 
 
-# Part 1 -- building a purely neutral model -------------------------------
+# Create a model, simulate tree sequence, get EIGENSTRAT data -------------
 
-# African ancestral population
-afr <- population("AFR", time = 65000, N = 5000)
+popZ <- population("popZ", time = 3000, N = 5000)
+popX <- population("popX", time = 1500, N = 5000, parent = popZ)
+popY <- population("popY", time = 1500, N = 5000, parent = popZ)
 
-# First migrants out of Africa
-ooa <- population("OOA", parent = afr, time = 60000, N = 5000, remove = 27000)
+model <- compile_model(list(popZ, popX, popY), generation_time = 1)
 
-# Eastern hunter-gatherers
-ehg <- population("EHG", parent = ooa, time = 28000, N = 5000, remove = 6000)
+schedule <- schedule_sampling(model, times = seq(3000, 0, by = -200), list(popZ, 10), list(popX, 10), list(popY, 10))
 
-# European population
-eur <- population("EUR", parent = ehg, time = 25000, N = 5000)
+# Use the function plot_model() to make sure that the model and the sampling schedule
+# are defined correctly (there's no such thing as too many sanity checks when doing research)
+plot_model(model, proportions = TRUE, samples = schedule)
 
-# Anatolian farmers
-ana <- population("ANA", time = 28000, N = 5000, parent = ooa, remove = 4000)
+ts <- msprime(model, samples = schedule, sequence_length = 50e6, recombination_rate = 1e-8, random_seed = 1702182272) %>%
+  ts_mutate(1e-8)
 
-# Yamnaya steppe population
-yam <- population("YAM", time = 8000, N = 5000, parent = ehg, remove = 2500)
+# Save the samples table to a data frame (and process it a bit for tidier plotting later)
+samples <- ts_samples(ts) %>% mutate(pop = factor(pop, levels = c("popZ", "popX", "popY")))
 
-# Define gene-flow events
-gf <- list(
-  gene_flow(from = ana, to = yam, rate = 0.75, start = 7500, end = 6000),
-  gene_flow(from = ana, to = eur, rate = 0.5, start = 6000, end = 5000),
-  gene_flow(from = yam, to = eur, rate = 0.6, start = 4000, end = 3500)
-)
+samples %>% group_by(pop) %>% count()
 
-# Compile all populations into a single slendr model object
-model <- compile_model(
-  populations = list(afr, ooa, ehg, eur, ana, yam),
-  gene_flow = gf, generation_time = 30
-)
 
-# Schedule the sampling from four European populations roughly before their
-# disappearance (or before the end of the simulation)
-schedule <- rbind(
-  schedule_sampling(model, times = 0, list(eur, 50)),
-  schedule_sampling(model, times = 6000, list(ehg, 50)),
-  schedule_sampling(model, times = 4000, list(ana, 50)),
-  schedule_sampling(model, times = 2500, list(yam, 50))
-)
 
-# Verify visually that our model is correct
-plot_model(model, samples = schedule)
-plot_model(model, proportions = TRUE)
 
 
+# Part 1 -- create EIGENSTRAT for different subsets of samples ------------
 
+# Our goal is to investigate how is PCA structure affected by different factors related
+# to sampling (sample sizes, dates of samples, etc.). Right now, our tree sequence contains
+# every single sample. What we will do now is create smaller subsets of the entire
+# tree sequence to only defined sets of samples using the process called "simplification",
+# then export the genomic data from the tree sequence into the EIGENSTRAT file format.
 
+# EIGENSTRAT with only "present-day" X and Y individuals
+subset <- filter(samples, pop %in% c("popX", "popY"), time == 0)
+subset
 
-# Part 2 -- simulating a tree sequence and computing Tajima's D -----------
+ts_XY0 <- ts_simplify(ts, simplify_to = subset$name)
+ts_eigenstrat(ts_XY0, "data/XY0")
 
-# Although the point of this exercise is to simulate selection, let's first
-# simulate a normal neutral model using slendr's msprime engine as a sanity check
-ts <- msprime(model, sequence_length = 10e6, recombination_rate = 1e-8, samples = schedule) %>%
-  ts_mutate(mutation_rate = 1e-8)
+# EIGENSTRAT with all X and Y individuals, present-day and ancient
+subset <- filter(samples, pop %in% c("popX", "popY"))
+subset
 
-# Inspect the table of all individuals recorded in our tree sequence
-ts_samples(ts)
+ts_XYall <- ts_simplify(ts, simplify_to = subset$name)
+ts_eigenstrat(ts_XYall, "data/XYall")
 
-# tskit functions in slendr generally operate on vectors (or lists) of individual
-# names, like those produced by ts_samples() above -- let's get get a vector of
-# such names using another helper function ts_names()
-samples <- ts_names(ts, split = "pop")
-samples
+# EIGENSTRAT with only "present-day" X,Y, and Z individuals
+subset <- filter(samples, time == 0)
+print(subset, n = Inf)
 
-# Compute genome-wide Tajima's D for each population -- note that we don't
-# expect to see any significant differences because no population experienced
-# natural selection (yet)
-ts_tajima(ts, sample_sets = samples)
+ts_XYZ0 <- ts_simplify(ts, simplify_to = subset$name)
+ts_eigenstrat(ts_XYZ0, "data/XYZ0")
 
+# EIGENSTRAT file with all individuals
+ts_eigenstrat(ts, "data/XYZall")
 
+# We can verify the contents of the EIGENSTRAT data by inspecting them in
+# the bash terminal (cat, less, etc.). Alternatively, we can use the R
+# package admixr to do this in R. For instance, we could do the following.
 
+library(admixr)
 
+eigen <- eigenstrat("data/XYZ0")
+eigen
 
-# Part 3 -- computing Tajima's D in windows -------------------------------
+read_ind(eigen)
+read_snp(eigen)
+read_geno(eigen)
 
-# Pre-compute genomic windows for window-based computation of Tajima's D
-windows <- round(seq(0, ts$sequence_length, length.out = 100))
-windows
 
-# Compute genome-wide Tajima's D for each population in individual windows
-tajima_wins <- ts_tajima(ts, sample_sets = samples, windows = windows)
-tajima_wins
-tajima_wins[1, ]$D
 
-# The numeric vector format of the result above is a bit annoying to work with
-# and visualize, so let's use some tidyverse "magic" to format it properly
-# using a helper function `process_tajima()` I wrote for you
-tajima_df <- process_tajima(tajima_wins)
-tajima_df
+# Part 2 -- computing and visualizing PCA patterns ------------------------
 
-# Now let's visualize the window-based Tajima's D along the simulated genome
-# using another helper function `plot_tajima()` (hint: we still don't expect
-# anything interesting here because we ran a purely neutral simulation, so this
-# is more like a control)
-plot_tajima(tajima_df)
+plot_pca("data/XY0", ts_XY0, color_by = "pop")
+plot_pca("data/XYall", ts_XYall, color_by = "time")
+plot_pca("data/XYZ0", ts_XYZ0, color_by = "pop")
 
+# The first two PCs can separate lineages from the split onwards, but can't
+# distinguish the ancestral Z individuals
+plot_pca("data/XYZall", ts, pc = c(1, 2), color_by = "time")
+plot_pca("data/XYZall", ts, pc = c(1, 2), color_by = "pop")
 
+# PC 2 vs 3 -- PC 2 doesn't really separate Z before and after the split, but
+# PC 3 does reveal the tree structure!
+plot_pca("data/XYZall", ts, pc = c(2, 3), color_by = "time")
+plot_pca("data/XYZall", ts, pc = c(2, 3), color_by = "pop")
 
+# PC 3 vs 4 -- PC 3 separates individuals before and after split, but doesn't
+# really separate the populations. PC 4 does, kind of?
+plot_pca("data/XYZall", ts, pc = c(3, 4), color_by = "time")
+plot_pca("data/XYZall", ts, pc = c(3, 4), color_by = "pop")
 
+# PC 4 vs 5 -- Hard to say what's going on, but it looks pretty. :)
+plot_pca("data/XYZall", ts, pc = c(4, 5), color_by = "time")
+plot_pca("data/XYZall", ts, pc = c(4, 5), color_by = "pop")
 
-# Part 4 -- simulating positive selection ---------------------------------
+# ... same!
+plot_pca("data/XYZall", ts, pc = c(5, 6), color_by = "time")
+plot_pca("data/XYZall", ts, pc = c(5, 6), color_by = "pop")
 
-# Our SLiM selection extension for slendr is in 'slim_extension.txt'. When
-# you inspect that file, you will se pretty much standard SLiM code with one
-# exception: the somewhat strange {{elements}} in curly brackets. Those
-# parameters of the selection model, which must be first "instantiated" using
-# the function `substitute_values()`.
 
-# First, let's simulate selection happening only in the EUR lineage.
-extension <- substitute_values(template = here::here("slim_extension.txt"),
-  origin_pop = "EUR",
-  s = 0.15,
-  onset_time = 12000
-)
 
-# When we take a look at the modified extension contents in the terminal using
-# something like the bash `less` command, we no longer see the {{parameters}} but
-# we see concrete values instead
-extension
 
-# Using the SLiM extension is simple -- we simply provide it as an additional
-# argument to the `compile_model()` function:
-model <- compile_model(
-  populations = list(afr, ooa, ehg, eur, ana, yam),
-  gene_flow = gf, generation_time = 30,
-  extension = extension   # <======== this is missing in the neutral example!
-)
-# We can finally run our selection simulation
 
-# This time our model not only produces a tree sequence, but it also generates
-# a table of allele frequencies in each population (see the file
-# 'slim_extension.txt'). We need to be able to load both of these files after
-# the simulation and thus need a path to a location we can find those files.
-# We can do this by specifying `path = TRUE`.
+# Bonus -- code up a PCA using another software you know ------------------
 
-# tstart <- Sys.time()
-path <- slim(model, sequence_length = 10e6, recombination_rate = 1e-8, samples = schedule, path = TRUE, random_seed = 59879916)
-# tend <- Sys.time()
-# tend - tstart # Time difference of 38.82014 secs
+# In addition to `ts_eigenstrat()`, slendr also provides an interface to
+# tskit's VCF functionality as its R function `ts_vcf()`. Use these tools
+# to test whether your own PCA software of choice produces the same results!
 
-# We can verify that the path not only contains a tree-sequence file but also
-# the table of allele frequencies.
-list.files(path)
-
-# Allele frequency trajectories -------------------------------------------
-
-traj_df <- read_trajectory(path)
-traj_df
-
-plot_trajectory(traj_df)
-
-# Comparing the trajectories side-by-side with the demographic model reveals
-# some obvious patterns of both selection and demographic history.
-plot_grid(
-  plot_model(model),
-  plot_trajectory(traj_df),
-  nrow = 1, rel_widths = c(0.7, 1)
-)
-
-# Tajima's D --------------------------------------------------------------
-
-# We need an additional call to `ts_recapitate()` to ensure that the tree
-# sequence is fully coalesced (and thus contains older polymorphisms)
-ts <- ts_read(file.path(path, "slim.trees"), model) %>%
-  ts_recapitate(Ne = 5000, recombination_rate = 1e-8) %>%
-  ts_mutate(mutation_rate = 1e-8)
-
-samples <- ts_names(ts, split = "pop")
-samples
-
-# Overall Tajima's D across the 10Mb sequence still doesn't reveal any significant
-# deviations even in case of selection (again, not entirely unsurprising)
-ts_tajima(ts, sample_sets = samples)
-
-# So let's look at the window-based computation again...
-windows <- as.integer(seq(0, ts$sequence_length, length.out = 100))
-
-# compute genome-wide Tajima's D for each population in individual windows
-tajima_wins <- ts_tajima(ts, sample_sets = samples, windows = windows)
-tajima_df <- process_tajima(tajima_wins)
-tajima_df
-
-plot_tajima(tajima_df)
-
-
-
-
-
-# Bonus exercises ---------------------------------------------------------
-
-
-
-
-
-# Bonus 1 -----------------------------------------------------------------
-
-# Vary the uniform recombination rate and observe what happens with Tajima's D
-# in windows along the genome.
-
-# Solution: just modify the value of the `recombination_rate =` argument provided
-# to the `slim()` function above.
-
-
-
-
-
-# Bonus 2 -----------------------------------------------------------------
-
-# Simulate the origin of the beneficial allele in the EHG population -- what
-# do the trajectories look like now? How does that change the Tajima's D
-# distribution along the genome in our European populations?
-
-# Solution -- use this extension in the `slim()` call, and repeat the rest.
-extension <- substitute_values(
-  template = "slim_extension.txt",
-  origin_pop = "EHG",
-  s = 0.1,
-  onset_time = 12000
-)
-model <- compile_model(
-  populations = list(afr, ooa, ehg, eur, ana, yam),
-  gene_flow = gf, generation_time = 30,
-  extension = extension
-)
-
-
-
-
-
-# Bonus 3 -- practice your tidyverse chops --------------------------------
-
-# Re-implement my `process_tajima()` function on your own. Hard mode: do this
-# using base R (no tidyverse allowed!).
